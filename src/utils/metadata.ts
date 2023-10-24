f8d4c92471cdf4d1d34819c6740c23b8c4800c15
Jalen Parham <jalenparham97@gmail.com>
2023-10-24 18:16:44 -0400

    Fixing broken utils for metadata

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,8 +1,23 @@
+import createImageUrlBuilder from '@sanity/image-url';
 import type { Metadata } from 'next';
 import type { Image } from 'sanity';
 
-import { urlForImage } from '@/sanity/sanity.helpers';
+import { dataset, projectId } from '@/sanity/sanity.api';
 import * as demo from '@/utils/demo.data';
+
+const imageBuilder = createImageUrlBuilder({
+  projectId: projectId || '',
+  dataset: dataset || '',
+});
+
+export const urlForImage = (source: Image) => {
+  // Ensure that source image contains a valid reference
+  if (!source?.asset?._ref) {
+    return undefined;
+  }
+
+  return imageBuilder?.image(source).auto('format').fit('max');
+};
 
 /**
  * All the shared stuff that goes into <head> on `(personal)` routes, can be be imported by `page.tsx` files and used by `generateMetadata` functions.
